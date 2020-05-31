04221af94f075bffb27688b06dd07b409ccc407e
JC Lee <jcleefw@gmail.com>
2020-05-31 11:07:31 +1000

    creating AUdio Context with init

--- src/modules/Metronome.test.ts
+++ src/modules/Metronome.test.ts
@@ -1,9 +1,15 @@
 import Metronome from './Metronome'
 
 describe('Metronome', () => {
-  it('should be true', () => {
+  it('when webaudio api is not supported, should throw error', () => {
     const metronome = new Metronome()
-    metronome.init(50)
-    expect(metronome.context).toBe('hello')
+
+    expect(() => metronome.init(50)).toThrowError(
+      'Web Audio API is not supported in this browser'
+    )
+
+    expect(() => metronome.getAudioContext()).toThrowError(
+      'Web Audio API is not supported in this browser'
+    )
   })
 })
--- src/modules/Metronome.ts
+++ src/modules/Metronome.ts
@@ -12,13 +12,21 @@ export default class Metronome {
   }
 
   init = (volume: number) => {
-    console.log('hello world')
     this.context = this.getAudioContext()
     this.volume = volume
+
     // this.gainNode = this.context.createGain()
   }
 
-  getAudioContext = () => {
-    return 'hello'
+  getAudioContext = (): AudioContext | Error => {
+    let context: AudioContext
+    // webkitAudioContext is old namespace of AudioContext
+    if (typeof AudioContext !== 'undefined') {
+      return new AudioContext()
+    } else if (typeof window.webkitAudioContext !== 'undefined') {
+      return new window.webkitAudioContext()
+    } else {
+      throw new Error('Web Audio API is not supported in this browser')
+    }
   }
 }
